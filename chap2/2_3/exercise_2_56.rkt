49f9900a9f64960cd523006d3a108af0bf7b4354
softtwilight <tjuyxl@gmail.com>
2022-12-26 20:50:17 +0900

    2.57

--- chap2/2_3/exercise_2_56.rkt
+++ chap2/2_3/exercise_2_56.rkt
@@ -1,7 +1,8 @@
 #lang racket
+(#%require "../../util/utils.rkt")
 ((sum? exp) (make-sum (deriv (addend exp) var)
                               (deriv (augend exp) var)))
-
+;; 2.56
 ((exponentiation? exp)
  (make-product
   (make-product (exponent exp)
@@ -18,3 +19,9 @@
         ((number? e2 0) 1)
         ((number? e2 1) e1)
         (else (list '** e1 e2))))
+
+;; 2.57
+(define (augend s)
+  (accumulate make-sum 0 (cddr s)))
+(define (multiplicand p)
+  (accumulate make-product 1 (cddr p)))
