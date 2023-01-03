86646bf8f5efa6b88b9a68b9ed3b484813e562ee
softtwilight <tjuyxl@gmail.com>
2023-01-03 15:52:19 +0900

    2.70

--- chap2/2_3/exercise_2_67.rkt
+++ chap2/2_3/exercise_2_67.rkt
@@ -115,3 +115,17 @@
 sample-tree
 new-tree
 (encode symbol-message new-tree)
+
+;; 2.70
+
+(define lyrics (list '(A 2) '(GET 2) '(SHA 3) '(WAH 1)
+                     '(BOOM 1) '(JOB 2) '(NA 16) '(YIP 9)))
+
+(define lyrics-tree (generate-huffman-tree lyrics))
+;lyrics-tree
+(define song 
+   '(GET A JOB SHA NA NA NA NA NA NA NA NA GET A JOB SHA NA NA NA NA NA NA NA NA WAH YIP YIP YIP YIP YIP YIP YIP YIP YIP SHA BOOM)) 
+;; 84
+(length (encode song lyrics-tree))
+;; minmun fixed-length code 108
+(* 3 (length song))
